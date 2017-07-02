83fb8eb7db7e2a79dd664b73901aa2edc071623c
root <root@localhost.localdomain>
2017-07-03 00:41:23 +0300

    Add pages to app

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 		resources :comments
 	end
 	root "posts#index"
+
+	get '/about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
